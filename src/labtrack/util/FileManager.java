a47c5850cb3866d6abfd08751131652a44a74e2b
Rufayed Rehan <rufayed.rehan@gmail.com>
2026-03-24 22:22:15 +0600

    Update

--- src/labtrack/util/FileManager.java
+++ src/labtrack/util/FileManager.java
@@ -4,8 +4,17 @@ import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Local file persistence utility for reading and writing data in CSV format.
+ * Abstracts standard IO operations and ensures files exist before access.
+ */
 public class FileManager {
 
+    /**
+     * Appends a line of data to a file.
+     * @param file The path to the file.
+     * @param data The data string to append.
+     */
     public static void write(String file, String data) {
         try (FileWriter fileWriter = new FileWriter(file, true)) {
             String lineToWrite = data + "\n";
@@ -15,6 +24,11 @@ public class FileManager {
         }
     }
 
+    /**
+     * Reads all non-empty lines from a file.
+     * @param file The path to the file.
+     * @return A list of strings, each representing a line in the file.
+     */
     public static List<String> readAllLines(String file) {
         List<String> lines = new ArrayList<>();
         File targetFile = new File(file);
@@ -37,6 +51,11 @@ public class FileManager {
         return lines;
     }
 
+    /**
+     * Overwrites a file with the provided list of lines.
+     * @param file The path to the file.
+     * @param lines The list of strings to write to the file.
+     */
     public static void overwrite(String file, List<String> lines) {
         try (FileWriter fileWriter = new FileWriter(file, false)) {
             for (String line : lines) {
